5fe87b5466872f7bd36f3d8e574614ee084b541c
deneiro <bauyrzhan.en@gmail.com>
2024-12-28 11:54:18 +0500

    Initial Commit

---
+++ Payroll Calculator/Payroll_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Payroll_CalculatorApp.swift
+//  Payroll Calculator
+//
+//  Created by Eldar Bauyrzhan on 28.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Payroll_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
